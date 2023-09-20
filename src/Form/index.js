f072f800764ce7db6c93c6c93cd13047863c6cb9
Wojciech Luczak <wojtek@alphabeteventz.pl>
2023-09-20 13:29:06 +0200

    form added interactions - input takes text

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,21 @@
+import React, {useState} from "react";
 import "./index.css";
-const Form = () => (
-  <form className="form" action="">
-    <input
-      className="form__input"
-      placeholder="Co jest do zrobienia?"
-      type="text"
-    />
-    <button className="form__button">Dodaj zadanie</button>
-  </form>
-);
+const Form = () => {
+const [newTaskContent, setNewTaskContent] = useState("");
+const onFormSubmit= (event) => {
+  event.preventDefault();
+};
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+      value={newTaskContent}
+        className="form__input"
+        placeholder="Co jest do zrobienia?"
+        onChange = {({target}) => setNewTaskContent(target.value)}
+        type="text"
+      />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  )
+};
 export default Form;
