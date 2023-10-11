5a179afaf8407ee741ca5bb3c006a397be459478
Wojciech Luczak <wojtek@alphabeteventz.pl>
2023-10-11 21:41:24 +0200

    added function getInitialTasks

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
+const getInitialTasks = () => {
+  const tasksFromLocalStorage = localStorage.getItem("tasks");
+  return tasksFromLocalStorage
+  ? JSON.parse(tasksFromLocalStorage)
+  : []
+};
+
 function App() {
   const [hideDone, setHideDone] = useState(false);
 
-const tasksFromLocalStorage = localStorage.getItem("tasks");
 
-  const [tasks, setTasks] = useState(
-    tasksFromLocalStorage
-    ? JSON.parse(tasksFromLocalStorage)
-    : []
-  );
+
+  const [tasks, setTasks] = useState(getInitialTasks  );
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
